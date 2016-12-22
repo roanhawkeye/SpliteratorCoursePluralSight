297df1f3d0ed1ef5702cccd954fba6260d44ae25
Daniel Martinez <roanhawkeye@gmail.com>
2016-12-22 15:38:00 -0600

    Add people file to read from and first draft of main class

--- src/org/example/CreatingSpliterator.java
+++ src/org/example/CreatingSpliterator.java
@@ -1,10 +1,33 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import org.example.model.Person;
+
 public class CreatingSpliterator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Path path = Paths.get("files/people.txt");
+		
+		try {
+			Stream<String> lines = Files.lines(path);
+			
+			Spliterator<String> lineSpliterator  = lines.spliterator();
+//			Spliterator<Person> personSpliterator = null;
+//			
+//			Stream<Person> people = StreamSupport.stream(personSpliterator, false);
+//			people.forEach(System.out::println);
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
